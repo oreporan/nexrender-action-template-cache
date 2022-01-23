965c5f9a466d5584fbb39e2c31c09eecf2215e46
Ore Poran <oreporan@gmail.com>
2022-01-23 13:03:17 +0200

    build

--- dist/buildCacheName.js
+++ dist/buildCacheName.js
@@ -4,9 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const path_1 = __importDefault(require("path"));
-const buildCacheName = (originalFileName, originalWorkPath, cacheDirName = 'template_cache') => {
+const buildCacheName = (originalFileName, originalWorkPath, cacheDirName) => {
     const fileName = path_1.default.basename(originalFileName);
-    return path_1.default.join(originalWorkPath, "..", cacheDirName, fileName);
+    return cacheDirName
+        ? path_1.default.join(cacheDirName, fileName)
+        : path_1.default.join(originalWorkPath, "..", "project_cache", fileName);
 };
 exports.default = buildCacheName;
 //# sourceMappingURL=buildCacheName.js.map
